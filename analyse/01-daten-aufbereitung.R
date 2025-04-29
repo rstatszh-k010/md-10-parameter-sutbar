9afa50a9e0f616efaa021de2c3ef9ca879ac4bf3
sutbar <barbara.sutter@lu.ch>
2025-04-29 12:56:37 +0000

    Berichtswebseite erstellen

--- analyse/01-daten-aufbereitung.R
+++ analyse/01-daten-aufbereitung.R
@@ -27,24 +27,25 @@ leerwohungen <- read_delim("https://www.web.statistik.zh.ch/ogd/data/KANTON_ZUER
 # Daten transformieren ----------------------------------------------------
 
 wohnungsbestand_klein <- wohnungsbestand |> 
-  # Beschreibung....
+  # Beschreibung: wählt Daten aus, damit weniger Daten vorhanden sind
   select(bfs_nr, gebiet_name, indikator_name, indikator_id, 
          indikator_jahr, indikator_value)
 
 leerwohungen_klein <-  leerwohungen |> 
-  # Beschreibung....
+  # Beschreibung: wählt Daten aus, damit weniger Daten vorhanden sind
   select(bfs_nr, gebiet_name, indikator_name, indikator_id,
          indikator_jahr, indikator_value)
 
 leerwohungen_wohnungsbestand <- wohnungsbestand_klein |> 
-  # Beschreibung....
+  # Beschreibung: er nimmt Wohnungsbestand_klein als Hauptdatensatz
   bind_rows(leerwohungen_klein) |> 
-  # Beschreibung....
+  # Beschreibung: er bindet die Daten von leerwohnung_klein ein (zusammenführen)
   filter(indikator_jahr < 2024) |> 
-  # Beschreibung....
+  # Beschreibung: er wählt alle jahre kleiner als 2024 aus
   filter(bfs_nr != 0) |>
-  # Beschreibung....
+  # Beschreibung: er nimmt alle bfs_nr ausser 0
   filter(!str_detect(gebiet_name, "bis"))
+  # Beschreibung: alle gemeinden mit dem wort "bis" werden rausgefiltert (Gememindefusionen!)
 
 # Daten speichern ----------------------------------------------------------
 
